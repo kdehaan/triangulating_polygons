3ea2d6e15e4446fd9ff27a3534010659f9a8989b
Kevin de Haan <kbehaan@gmail.com>
2020-11-09 19:44:01 -0700

    graph file reading first implementation

--- triangles.py
+++ triangles.py
@@ -9,8 +9,10 @@ class Vertex:
     value = None
     neighbours = []
 
-    def __init__(self, key):
+    def __init__(self, key, value, neighbours):
         self.key = key
+        self.value = value
+        self.neighbours = neighbours
 
     def addNeighbour(self, point):
         self.neighbours.append(point)
@@ -19,17 +21,25 @@ class Vertex:
 
 
 def readCSV(fileLocation=DEFAULT_GRAPH):
+    points = {}
+
     with open(fileLocation, newline='') as graphFile:
         graphReader = csv.reader(graphFile, delimiter=",")
+
+        next(graphReader, None) #skips csv header
         for row in graphReader:
-            print(row)
+            point = Vertex(row[0], row[1], row[2])
+            points[point.key] = point
+        return points
 
 
 
 def main():
-    readCSV()
-
-
+    graphPoints = readCSV()
+ 
+    for key in graphPoints:
+        point = graphPoints[key]
+        print(point.key, point.value, point.neighbours)
 
 
 
