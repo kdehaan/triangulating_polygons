cd31560a4817750bcc806d676663fd62ba57ddd0
Kevin de Haan <kbehaan@gmail.com>
2020-11-09 19:21:46 -0700

    setting up readcsv and the vertex class

--- triangles.py
+++ triangles.py
@@ -1,11 +1,33 @@
+import csv
+import os
+
+DEFAULT_GRAPH = "default.csv"
+
+class Vertex:
+    """A point in an undirected graph"""
+    key = ""
+    value = None
+    neighbours = []
+
+    def __init__(self, key):
+        self.key = key
+
+    def addNeighbour(self, point):
+        self.neighbours.append(point)
 
 
 
+
+def readCSV(fileLocation=DEFAULT_GRAPH):
+    with open(fileLocation, newline='') as graphFile:
+        graphReader = csv.reader(graphFile, delimiter=",")
+        for row in graphReader:
+            print(row)
 
 
 
 def main():
-    print("placeholder")
+    readCSV()
 
 
 
