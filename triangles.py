c99c36ff9a623aef9081714e4b1a4a8a6cf9a34b
Kevin de Haan <kbehaan@gmail.com>
2020-11-09 22:25:30 -0700

    essentially done (?) will come back to check after I've given it some time

--- triangles.py
+++ triangles.py
@@ -27,6 +27,7 @@ class Vertex:
 def readCSV(fileLocation=DEFAULT_GRAPH):
     points = {}
     edgePoints = set()
+    valueTypes = set()
 
     with open(fileLocation, newline='') as graphFile:
         graphReader = csv.reader(graphFile, delimiter=",")
@@ -37,7 +38,8 @@ def readCSV(fileLocation=DEFAULT_GRAPH):
             points[point.key] = point
             if point.value is not None:
                 edgePoints.add(point.key)
-        return points, edgePoints
+                valueTypes.add(point.value)
+        return points, edgePoints, valueTypes
 
 
 
@@ -47,44 +49,51 @@ def findMinPairs(graph, edges):
     # currentKey = next(iter(edges)) # get arbitrary point from the edge set
     
     # at this point I messed around with intelligently reading every other 
-    # point so as to minimize reads, but I realized it only made it O(n/2) 
+    # point so as to minimize set operations, but I realized it only made it O(n/2) 
     # from O(n) and decided it was more trouble that it was worth
 
     for point in edges:
         edgeNeighbours = graph[point].getEdgeNeighbours(edges)
         for neighbour in edgeNeighbours:
-            pairType = getPairType(graph[neighbour], graph[point])
+            if (graph[neighbour].value == graph[point].value):
+                continue
+            pairType = getPairKey(graph[neighbour].value, graph[point].value)
 
             if (pairType in edgePairs and type(edgePairs[pairType]) is set):
                 edgePairs[pairType].add(getPairKey(point, neighbour))
             else:
                 edgePairs[pairType] = set([getPairKey(point, neighbour)])
     
-    print(edgePairs)
-    # while True:
-
-
-    #     print(pairPartners)
-    #     print(currentKey, chosenPartner, unchosenPartner)
-    #     print(edgePairs)
-    #     print("diff", pairPartners.difference(set(unchosenPartner)))
-
-    #     break
-
-
-    # print(currentKey)
-    # print(pairPartners)
     
+    return edgePairs
 
-def getPairType(pointA, pointB):
-    return getPairKey(pointA.value, pointB.value)
+    
 
 def getPairKey(a, b):
     return "({})".format(" ".join(sorted((a, b))))
 
+
+def getMinTriangles(edgePairs, valueTypes):
+    minVal = float('inf')
+    minPair = None
+    for pair in edgePairs:
+        if len(edgePairs[pair]) < minVal:
+            minVal = len(edgePairs[pair])
+            minPair = pair
+   
+    fillType = valueTypes - set(minPair.strip("()").split(" "))
+    return minVal, fillType
+
+
 def main():
-    graphPoints, edgePoints = readCSV()
-    findMinPairs(graphPoints, edgePoints)
+    graphPoints, edgePoints, valueTypes = readCSV()
+    edgePairs = findMinPairs(graphPoints, edgePoints)
+    minTriangles, fillType = getMinTriangles(edgePairs, valueTypes)
+
+    print("The minimum number of completed triangles possible in this polygon is {}. One way to accomplish this is to fill points all points with the value {}".format(minTriangles, fillType))
+
+    
+
     # for key in graphPoints:
     #     point = graphPoints[key]
     #     print(point.key, point.value, point.neighbours, point.isEdge)
